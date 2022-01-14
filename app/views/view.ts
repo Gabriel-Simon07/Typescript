8488905b69ecd730ba577e50030837c605373581
Gabriel Simon <gabrielsimon775@gmail.com>
2022-01-13 21:35:45 -0300

    Implementada duas novas opções de compilação no tsconfig.json e ativado o StrictNullchecks

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,10 +1,15 @@
 export abstract class View<T> {
 
-    protected elemento: HTMLLIElement
+    protected elemento: HTMLElement
     private escapar = false;
 
     constructor(seletor: string, escapar?:boolean) {
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if(elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM`)
+        }
         if(escapar) {
             this.escapar = escapar;
         }
